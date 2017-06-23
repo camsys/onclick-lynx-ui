d0028908ea50d26199e3d8816ef9a20d9e5dc414
mmaranda-cs <mmaranda@camsys.com>
2017-06-23 11:58:28 -0400

    Added an initial landing page. It needs to be styled. But helps work through the flow currently designed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule }       from '@angular/http';
 
 import {AppRoutingModule}                   from './app-routing.module';
 import {AppComponent}                       from './app.component';
+import {HelpMeFind}                         from './help_me_find/help-me-find.component';
 import {CategoriesFor211Component}          from "./211_services/211_service_categories/categories-for-211.component";
 import {ReferNet211Service}                 from "./211_services/refer-net-211-services.service";
 import {SubcategoriesFor211Component}       from "./211_services/211_service_subcategories/subcategories-for-211.component";
@@ -25,6 +26,7 @@ import {Globals}                            from "./globals";
     SubcategoriesFor211Component,
     SubcategoryLinksFor211Component,
     MatchListsFor211Component,
+    HelpMeFind,
   ],
   providers: [ReferNet211Service, Globals],
   bootstrap: [AppComponent]
---
+++ src/app/help_me_find/help-me-find.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'help-me-find',
+  templateUrl: './help-me-find.component.html',
+  styleUrls: ['../app.component.css']
+})
+
+export class HelpMeFind {
+  title = 'VTRAX';
+}
+/**
+ * Created by mmaranda on 6/23/17.
+ */

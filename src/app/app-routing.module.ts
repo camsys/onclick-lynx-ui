8e0a2794ab7282ab5ad4ac76515286243f7f3901
mmaranda-cs <mmaranda@camsys.com>
2017-06-23 11:46:25 -0400

    Did not update the routing or app module to incldue new classes for the match_list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import {CategoriesFor211Component}          from './211_services/211_service_categories/categories-for-211.component';
 import {SubcategoriesFor211Component}       from './211_services/211_service_subcategories/subcategories-for-211.component';
-import {SubcategoriLinksFor211Component}    from './211_services/211_service_subcategory_links/subcategory-links-for-211.component';
+import {SubcategoryLinksFor211Component}    from './211_services/211_service_subcategory_links/subcategory-links-for-211.component';
+import {MatchListsFor211Component}    from './211_services/211_services_match_lists/match-lists-for-211.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: '211Categories',                    component: CategoriesFor211Component },
   { path: '211Subcategories/:Category_Name',  component: SubcategoriesFor211Component },
-  { path: '211SubcategoryLinks/:Category_ID',  component: SubcategoriLinksFor211Component },
+  { path: '211SubcategoryLinks/:Category_ID',  component: SubcategoryLinksFor211Component },
+  { path: '211MatchLists/:name', component: MatchListsFor211Component },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import {AppComponent}                       from './app.component';
 import {CategoriesFor211Component}          from "./211_services/211_service_categories/categories-for-211.component";
 import {ReferNet211Service}                 from "./211_services/refer-net-211-services.service";
 import {SubcategoriesFor211Component}       from "./211_services/211_service_subcategories/subcategories-for-211.component";
-import {SubcategoriLinksFor211Component}    from './211_services/211_service_subcategory_links/subcategory-links-for-211.component';
+import {SubcategoryLinksFor211Component}    from './211_services/211_service_subcategory_links/subcategory-links-for-211.component';
+import {MatchListsFor211Component}          from './211_services/211_services_match_lists/match-lists-for-211.component';
 import {Globals}                            from "./globals";
 
 @NgModule({
@@ -22,7 +23,8 @@ import {Globals}                            from "./globals";
     AppComponent,
     CategoriesFor211Component,
     SubcategoriesFor211Component,
-    SubcategoriLinksFor211Component,
+    SubcategoryLinksFor211Component,
+    MatchListsFor211Component,
   ],
   providers: [ReferNet211Service, Globals],
   bootstrap: [AppComponent]
